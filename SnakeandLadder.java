212c4c6c380d59231b18d23d41de0f685cc84274
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-23 13:58:24 +1400

    Refactor - program2 functionality added

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,6 +1,8 @@
-public class SnakeandLadder{
-
-     public static void main(String []args){
-        System.out.println("SnakeandLadder");
-     }
-}
+public class SnakeandLadder {
+ public static void main(String[] args) {
+     int Startposition = 0 ;
+     int dies = (int) Math.floor(((Math.random() * 10) % 6) + 1);
+     System.out.println("Playerposition : " +Startposition);
+     System.out.println("Number of dies are : " +dies); 
+  } 
+} 
