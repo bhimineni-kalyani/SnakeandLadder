9c4c64b86872b60427c58902d918fdaf9ff6c227
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-23 14:07:47 +1400

    Refactor - program4 fuunctionality added

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,6 +1,41 @@
-public class SnakeandLadder{
-
-     public static void main(String []args){
-        System.out.println("SnakeandLadder");
-     }
+public class SnakeandLadder {
+    public static final int Snake = 1;
+    public static final int Ladder = 2;
+    
+ public static void main(String args[]) {
+    int Playerposition = 10;
+    System.out.println("Playerposition : " +Playerposition);
+    
+    while(Playerposition < 100) {
+        
+         int Option = (int) Math.floor((Math.random() * 10) % 3);
+         int dies = (int) Math.floor(((Math.random() * 10) % 6) + 1);
+         
+      switch (Option) {
+        case Ladder:
+            System.out.println("Number of dies are : " +dies); 
+            Playerposition += dies ;
+            if (Playerposition >= 100) {
+               System.out.println("Player reaches the winning position");
+            }
+            else{
+                   System.out.println("Ladder,player moves ahead by the number of position : " +Playerposition);
+            }
+            break;
+        
+        case Snake:
+            System.out.println("Number of dies are : " +dies); 
+            Playerposition -= dies ;
+            if(Playerposition < 0){
+                Playerposition = 0;
+            }
+            System.out.println("Snake,player moves behind by the number of position : " +Playerposition);
+            break;
+                    
+        default:
+            Playerposition = Playerposition;
+            System.out.println("No play,player stays in the same position : " +Playerposition);
+      }
+    }
+ }
 }
